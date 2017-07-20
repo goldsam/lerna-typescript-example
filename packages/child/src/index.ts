a57291bb1295a676f3b7fd63dbc3c9f88188b0f4
Sam Goldmann <sam.goldmann@gmail.com>
2017-07-19 20:06:37 -0400

    Hoisted common dependencies Added typedoc support

--- packages/child/src/index.ts
+++ packages/child/src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * Returns the sum of two numbers.
+ * @param x First number.
+ * @param y Second number.
+ * @return Sum of arguments.
+ */
 export function add(x: number, y: number): number {
   return x + y;
 }
